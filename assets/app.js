2d2928ef5413392e134fa290d7d5a6e9e94dacd0
Tim Goudriaan <tim@onlinq.nl>
2021-09-15 23:26:27 +0200

    Refactor existing assets to a Webpack build

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,36 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.scss in this case)
+import 'bootstrap';
+
+// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+window.addEventListener('load', () => {
+  const fruitsForms = document.querySelectorAll('[data-fruits-form]');
+
+  fruitsForms.forEach(fruitsForm => {
+    const fruitsResult = fruitsForm.querySelector('[data-fruits-result]');
+    const fruitsResultList = fruitsForm.querySelector('[data-fruits-result]').querySelector('ul');
+
+    fruitsForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      fruitsResult.style.display = null;
+
+      fruitsResultList.innerHTML = '';
+
+      const formData = new FormData(fruitsForm);
+
+      for (const pair of formData.entries()) {
+        const listItem = document.createElement('li');
+        listItem.innerText = pair[1];
+
+        fruitsResultList.append(listItem);
+      }
+    });
+  });
+});
--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Onlinq\FormCollectionBundle\OnlinqFormCollectionBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
@@ -10,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Onlinq\FormCollectionBundle\OnlinqFormCollectionBundle::class => ['all' => true],
 ];
